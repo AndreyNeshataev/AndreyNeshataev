fca3f64b9af0f1c1bc8c4d02848d958eaa95226b
AndreyNeshataev <98023165+AndreyNeshataev@users.noreply.github.com>
2022-12-13 15:22:27 +0500

    Delete 12_DjangoCaching directory

--- 12_DjangoCaching/djcaching/app_shops/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from app_shops.views import home_page, shops_list, ShopPage  # , shop_page
-
-urlpatterns = [
-    path('', home_page, name='home'),
-    path('shops_list', shops_list, name='shops_list'),
-    path('<int:pk>/shop_page', ShopPage.as_view(), name='shop_page'),
-]
