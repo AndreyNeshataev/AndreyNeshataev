d68dbca687db61871937eb7d374225b92fff08ad
AndreyNeshataev <98023165+AndreyNeshataev@users.noreply.github.com>
2022-12-13 15:21:59 +0500

    Delete 10_Testing directory

--- 10_Testing/djtesting/app_file/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app_file.models import AppFile
-
-# Register your models here.
-
-# @admin.register(Item)
-# class AppFileAdmin(admin.ModelAdmin):
-#     list_display = ('code', 'price')
