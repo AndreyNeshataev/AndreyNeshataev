cfd90e762d6b458448f048d9ed4ccc434d1b6ea5
AndreyNeshataev <98023165+AndreyNeshataev@users.noreply.github.com>
2022-12-13 15:22:36 +0500

    Delete 13_IntroductionToDjangoRESTFramework/djlibrary directory

--- 13_IntroductionToDjangoRESTFramework/djlibrary/app_library/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from app_library.views import BookList, BookDetail, AuthorList, AuthorDetail
-
-urlpatterns = [
-    path('books/', BookList.as_view(), name='books_list'),
-    path('<int:pk>/book_detail/', BookDetail.as_view(), name='book_detail'),
-    path('author/', AuthorList.as_view(), name='author_list'),
-    path('<int:pk>/author_detail/', AuthorDetail.as_view(), name='author_detail'),
-]
