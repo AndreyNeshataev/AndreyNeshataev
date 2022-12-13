25f2755295730de558f1e689bd46e7ac608caa25
AndreyNeshataev <98023165+AndreyNeshataev@users.noreply.github.com>
2022-12-13 15:21:47 +0500

    Delete 09_Files/djfiles directory

--- 09_Files/djfiles/app_file/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppFileConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_file'
-    verbose_name = 'Файл'
--- 09_Files/djfiles/app_users/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppUsersConfig(AppConfig):
-    name = 'app_users'
-    verbose_name = 'Пользователь'
